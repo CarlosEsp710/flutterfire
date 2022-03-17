8cbf2265181c22c1199fcde1470cb048abebcd43
Carlos Espejel <carlosespejel71@gmail.com>
2022-03-17 11:31:42 -0600

    Diseño de flutterfire-codelabs

---
+++ android/app/src/main/kotlin/com/cespejel/flutter_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cespejel.flutter_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:flutter_firebase/src/widgets/widgets.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Meetup',
+      theme: ThemeData(
+        buttonTheme: Theme.of(context).buttonTheme.copyWith(
+              highlightColor: Colors.deepPurple,
+            ),
+        primarySwatch: Colors.deepPurple,
+        textTheme: GoogleFonts.robotoTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Firebase Meetup'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Image.asset('assets/codelab.png'),
+          const SizedBox(height: 8),
+          const IconAndDetail(
+            icon: Icons.calendar_today,
+            detail: 'October 30',
+          ),
+          const IconAndDetail(
+            icon: Icons.location_city,
+            detail: 'San Francisco',
+          ),
+          const Divider(
+            height: 8,
+            thickness: 1,
+            indent: 8,
+            endIndent: 8,
+            color: Colors.grey,
+          ),
+          const Header(heading: "What we'll be doing"),
+          const Paragraph(
+            content: 'Join us for a day full of Firebase Workshops and Pizza!',
+          ),
+        ],
+      ),
+    );
+  }
+}
